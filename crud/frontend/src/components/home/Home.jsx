05c5b6e79a29a57e879d16bc0676ec0b6e0a31a5
Francisco Franco <franciscofrancodev@gmail.com>
2021-09-18 22:13:38 -0300

    Primeiro commit

---
+++ crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Main from '../template/Main'
+
+export default props =>
+
+    <Main icon="home" title="Início"
+        subtitle="Cadastro de usuários por setor">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para cadastro de cargos e setores</p>
+    </Main>
---
+++ crud/frontend/src/components/template/Nav.jsx
@@ -0,0 +1,21 @@
+import './Nav.css'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+export default props =>
+
+
+    <aside className="menu area">
+        <nav className="menu">
+            <Link to="/">
+                <i className="fa fa-home"></i> INÍCIO
+            </Link>
+            <Link to="/users">
+                <i clLinkssName="fa fa-users"></i>SETORES
+            </Link>
+        </nav>
+        <div className="accordion">
+            
+        </div>
+    </aside>
+
